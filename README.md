73b8d98aa564d1967f1f44fcd740d43855c7dbdf
Rik Smeets <30838294+rik-smeets@users.noreply.github.com>
2018-04-21 09:57:40 +0200

    Add script and extend README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# markdown-relative-link-validator
-Validate the relative links in your Markdown files using PowerShell.
+# Markdown relative link validator
+Validate the relative links in your Markdown files with PowerShell! 
+Broken relative links are simply written to the default output as a table grouped by file. 
+Since it just checks whether relative links actually exist on the file system, it works for any type of relative link (i.e. to other Markdown files, images etc.).
+
+## Usage
+Get the source and either:
+- Run the script and enter path when prompted;
+- Call the script with a path as the parameter.
+
+## Contributions are welcome
+This simple script was quickly written when the author found it rather bothersome to manually validate relative links when reorganizing the folder structure of a big repository with lots of Markdown files. 
+
+Because of this specific purpose, it's also rather basic. Feel free to improve or extend it: pull requests are very welcome! 
+
+## Thanks to...
+- Stack Overflow user [sln](https://stackoverflow.com/users/557597/sln) for [sharing the regular expression](https://stackoverflow.com/a/44511243) used in this script when answering [this question](https://stackoverflow.com/questions/44511043/regex-to-match-local-markdown-links/).
---
+++ Test-RelativeMarkdownLinks.ps1
@@ -0,0 +1,36 @@
+﻿param(
+    [Parameter(Mandatory = $True)]
+    [string]$folderPath
+)
+
+while (!(Test-Path -Path $folderPath)) {
+    $folderPath = Read-Host "Enter a valid, existing and accessible folder path:"
+}
+
+$invalidLinks = @()
+Get-ChildItem -Path $folderPath -Filter *.md -Recurse | ForEach-Object {
+    $fileDirectory = $_.Directory
+    $fileFullName = $_.FullName
+    $fileText = Get-Content $fileFullName
+
+    $regexResult = $fileText | Select-String -Pattern '((!?\[[^\]]*?\])\((?:(?!http|www\.|\#|\.com|\.net|\.info|\.org).)*?\))'
+    
+    $regexResult.Matches | Where-Object { $_.Groups } | ForEach-Object {
+        $fullRelativeLink = $_.Groups[0].Value;
+        $lastPartOfPath = $fullRelativeLink.Split('(')[-1].Trim('(', ')').Replace("%20", " ")
+        
+        $lastPartOfPath | ForEach-Object {
+            $fullPath = Join-Path -Path $fileDirectory -ChildPath $_
+            if (!(Test-Path -Path $fullPath)) {
+                $invalidLinks += @{'File' = $fileFullName; 'Invalid relative link' = $fullRelativeLink}
+            }
+        }
+    }
+}
+
+if ($invalidLinks.Length -gt 0) {
+    $invalidLinks.ForEach( {[PSCustomObject]$_}) | Format-Table -Property "Invalid relative link" -AutoSize -GroupBy File
+}
+else {
+    Write-Output "No invalid relative links found."
+}
